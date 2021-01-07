6cdbb990b8f06f4283a840fec0a37c0b2c489e95
LAngel <luiscartaya27@hotmail.com>
2021-01-06 22:12:39 -0400

    maquetacion casi finalizada

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,51 @@
 // let d = ${document}.ready(function(){
 //     alert('hola')
-// })
-$(document).ready( function () {
-    $('[data-toggle="tooltip"]').tooltip()
+// });
+
+
+// let $product__tree = document.getElementById('tree')
+// let $button = $product__tree.querySelector('.btn')
+// let $card = $product__tree.querySelector('.card')
+// console.log($card)
+
+// function toggle(object, r) {
+//     this.object =  object
+//     this.r = r
+// }
+
+// toggle.prototype.remove = function () {
+//     this.object.addEventListener('click', function (event) {
+//         console.log(event)
+//     })
+// }
+
+// toggle.prototype.value = function () {
     
-    
-});
+// }
+
+
+
+// const remove = new toggle($button,$card)
+// remove.value()
+
+
+
+$(document).ready(() => {
+    let $header = $('.header').find('.navbar') // selector
+    $('.header .navbar-toggler').on('click', () => {
+        create.hide()
+    })
+    let create = $('<a>', { href: 'https://platzi.com',target: '_blank',html: 'hello world'}).appendTo($header) // createElement
+
+    console.log(create.attr('href'))
+
+
+    setTimeout(() => {
+        create.css({
+            'font-size': '100px', // add style
+        })
+        create.html('<h1>jeje</h1>') // innerHtml
+    }, 5000);
+
+})
+
