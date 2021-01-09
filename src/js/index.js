4b01497ccf5b634c8a4d7fa4ba5e0c51caccaaaa
LAngel <luiscartaya27@hotmail.com>
2021-01-09 01:43:35 -0400

    corrigiendo errores

--- src/js/index.js
+++ src/js/index.js
@@ -3,51 +3,38 @@
 // });
 
 
-// let $product__tree = document.getElementById('tree')
-// let $button = $product__tree.querySelector('.btn')
-// let $card = $product__tree.querySelector('.card')
-// console.log($card)
-
-// function toggle(object, r) {
-//     this.object =  object
-//     this.r = r
-// }
-
-// toggle.prototype.remove = function () {
-//     this.object.addEventListener('click', function (event) {
-//         console.log(event)
-//     })
-// }
-
-// toggle.prototype.value = function () {
-    
-// }
+$(document).ready(() => {
+    let $header = $('.header').find('.navbar') // selector
+    $('.header .navbar-toggler').on('click', () => {
+        create.hide()
+    })
+    let create = $('<a>', {
+        href: 'https://platzi.com',
+        target: '_blank',
+        html: 'hello world'
+    }).appendTo($header) // createElement
 
 
+    create.css({
 
-// const remove = new toggle($button,$card)
-// remove.value()
+        'font-size': '100px', // add style
 
+    })
 
-
-// $(document).ready(() => {
-//     let $header = $('.header').find('.navbar') // selector
-//     $('.header .navbar-toggler').on('click', () => {
-//         create.hide()
-//     })
-//     let create = $('<a>', { href: 'https://platzi.com',target: '_blank',html: 'hello world'}).appendTo($header) // createElement
-
-//     // create.css({
-//     //     'font-size': '100px', // add style
-//     // })
-    
-//     // console.log(create.attr('href')) // attributes 
-//     // console.log(create.attr('href', 'google')) // attribute set(cambiar)
+    console.log(create.attr('href')) // attributes 
+    console.log(create.attr('href', 'google')) // attribute set(cambiar)
 
 
-//     // setTimeout(() => {
-//     //     create.html('<h1>jeje</h1>') // innerHtml
-//     // }, 5000);
+    $.ajax({
+        url: 'https://randomuser.me/api/?results=1',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data.results[0].id)
+        },
+        error: function (err) {
+            console.log(`${err} not found ${data}`)
+        }
+        
+    });
 
-// })
-
+})
