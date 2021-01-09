f13344b2ad219f0318ed91872b92fce89ddb8aa0
LAngel <luiscartaya27@hotmail.com>
2021-01-09 13:30:12 -0400

    maquetando el header

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,40 @@
-// let d = ${document}.ready(function(){
-//     alert('hola')
-// });
+// // let d = ${document}.ready(function(){
+// //     alert('hola')
+// // });
 
 
-$(document).ready(() => {
-    let $header = $('.header').find('.navbar') // selector
-    $('.header .navbar-toggler').on('click', () => {
-        create.hide()
-    })
-    let create = $('<a>', {
-        href: 'https://platzi.com',
-        target: '_blank',
-        html: 'hello world'
-    }).appendTo($header) // createElement
+// $(document).ready(() => {
+//     let $header = $('.header').find('.navbar') // selector
+//     $('.header .navbar-toggler').on('click', () => {
+//         create.hide()
+//     })
+//     // let create = $('<a>', {
+//     //     href: 'https://platzi.com',
+//     //     target: '_blank',
+//     //     html: 'hello world'
+//     // }).appendTo($header) // createElement
 
 
-    create.css({
+//     // create.css({
 
-        'font-size': '100px', // add style
+//     //     'font-size': '100px', // add style
 
-    })
+//     // })
 
-    console.log(create.attr('href')) // attributes 
-    console.log(create.attr('href', 'google')) // attribute set(cambiar)
+//     console.log(create.attr('href')) // attributes 
+//     console.log(create.attr('href', 'google')) // attribute set(cambiar)
 
 
-    $.ajax({
-        url: 'https://randomuser.me/api/?results=1',
-        dataType: 'json',
-        success: function (data) {
-            console.log(data.results[0].id)
-        },
-        error: function (err) {
-            console.log(`${err} not found ${data}`)
-        }
+//     $.ajax({
+//         url: 'https://randomuser.me/api/?results=1',
+//         dataType: 'json',
+//         success: function (data) {
+//             console.log(data.results[0].id)
+//         },
+//         error: function (err) {
+//             console.log(`${err} not found ${data}`)
+//         }
         
-    });
+//     });
 
-})
+// })
